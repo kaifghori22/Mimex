1648a00d8e899377884cdc13fd7d489b483bc657
Faizan Ahmed Khan R K <faizanahmedkhan005@gmail.com>
2023-04-05 23:47:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mimex/SplashScreen.java
@@ -0,0 +1,22 @@
+package com.example.mimex;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },5000);
+    }
+}
