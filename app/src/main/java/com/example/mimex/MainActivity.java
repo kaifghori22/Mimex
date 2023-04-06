1a0a9552d364e28fe9062b930520cf3d82f35b09
Faizan Ahmed Khan R K <faizanahmedkhan005@gmail.com>
2023-04-07 00:03:29 +0530

    Side Menu Integration

--- app/src/main/java/com/example/mimex/MainActivity.java
+++ app/src/main/java/com/example/mimex/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.mimex;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.navigationOpen,R.string.navigationClose);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
