c9b1ee06f69d4ed696d2febab2c964bacf0a2d90
Faizan Ahmed Khan R K <faizanahmedkhan005@gmail.com>
2023-04-07 00:46:03 +0530

    Camera and side menu integration

--- app/src/main/java/com/example/mimex/MainActivity.java
+++ app/src/main/java/com/example/mimex/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    Camera camera;
+    FrameLayout frameLayout;
+    ShowCamera showCamera;
+    ImageButton add,spacebar,speak,clear;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
 
@@ -24,5 +31,20 @@ public class MainActivity extends AppCompatActivity {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.navigationOpen,R.string.navigationClose);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        frameLayout = (FrameLayout) findViewById(R.id.camLayout);
+
+        //open the camera
+
+        camera = Camera.open();
+
+        showCamera = new ShowCamera(this, camera);
+        frameLayout.addView(showCamera);
+
+        add = findViewById(R.id.add);
+        spacebar = findViewById(R.id.spacebar);
+        speak = findViewById(R.id.speak);
+        clear = findViewById(R.id.clear);
     }
 }
+
